e65d6f45ca83179a3c154dd34b1114a3a3f55e5a
JY <ithinkinx7@gmail.com>
2022-09-30 11:58:35 +0800

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
-import { ActionPanel, Action, List, Icon, showToast, Toast } from "@raycast/api";
-import { useState, useEffect, useRef, useCallback } from "react";
-import fetch, { AbortError } from "node-fetch";
+import { ActionPanel, Action, List, Icon, showToast, Toast } from "@raycast/api"
+import { useState, useEffect, useRef, useCallback } from "react"
+import fetch, { AbortError } from "node-fetch"
 import { URLSearchParams } from "url"
-import { request } from 'urllib';
+import { request } from "urllib"
 
-const [SEARCHURL, STARTURL] = ["http://localhost:48065/wechat/search", "http://localhost:48065/wechat/start"];
+const [SEARCHURL, STARTURL] = [
+  "http://localhost:48065/wechat/search",
+  "http://localhost:48065/wechat/start"
+]
 
 export default function Command() {
-  const { state, search } = useSearch();
+  const { state, search } = useSearch()
 
   return (
     <List
@@ -18,18 +21,21 @@ export default function Command() {
     >
       <List.Section title="Contacts:" subtitle={state.items.length + ""}>
         {state.items.map((searchResult) => (
-          <SearchListItem key={searchResult.title} searchResult={searchResult} />
+          <SearchListItem
+            key={searchResult.title}
+            searchResult={searchResult}
+          />
         ))}
       </List.Section>
     </List>
-  );
+  )
 }
 
 function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
   async function startWeChat() {
-    await request(searchResult.url);
+    await request(searchResult.url)
   }
-  const title = searchResult.title || searchResult.subtitle || searchResult.arg;
+  const title = searchResult.title || searchResult.subtitle || searchResult.arg
   return (
     <List.Item
       title={title}
@@ -50,90 +56,110 @@ function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
             />
           </ActionPanel.Section>
           <ActionPanel.Section>
-            <Action.OpenInBrowser title="Feature Request" url="https://github.com/Jax0rz/wechat" shortcut={{ modifiers: ["cmd"], key: "h" }} />
+            <Action.OpenInBrowser
+              title="Feature Request"
+              url="https://github.com/Jax0rz/wechat"
+              shortcut={{ modifiers: ["cmd"], key: "h" }}
+            />
           </ActionPanel.Section>
         </ActionPanel>
       }
     />
-  );
+  )
 }
 
 function useSearch() {
-  const [state, setState] = useState<SearchState>({ items: [], isLoading: true });
-  const cancelRef = useRef<AbortController | null>(null);
+  const [state, setState] = useState<SearchState>({
+    items: [],
+    isLoading: true
+  })
+  const cancelRef = useRef<AbortController | null>(null)
 
   const search = useCallback(
     async function search(searchText: string) {
-      cancelRef.current?.abort();
-      cancelRef.current = new AbortController();
+      cancelRef.current?.abort()
+      cancelRef.current = new AbortController()
       setState((oldState) => ({
         ...oldState,
-        isLoading: true,
-      }));
+        isLoading: true
+      }))
       try {
-        const items = await performSearch(searchText, cancelRef.current.signal);
+        const items = await performSearch(searchText, cancelRef.current.signal)
         setState((oldState) => ({
           ...oldState,
           items: items,
-          isLoading: false,
-        }));
+          isLoading: false
+        }))
       } catch (error) {
         setState((oldState) => ({
           ...oldState,
-          isLoading: false,
-        }));
+          isLoading: false
+        }))
 
         if (error instanceof AbortError) {
-          return;
+          return
         }
 
-        console.error("search error", error);
-        showToast({ style: Toast.Style.Failure, title: "Could not perform search", message: String(error) });
+        console.error("search error", error)
+        showToast({
+          style: Toast.Style.Failure,
+          title: "Could not perform search",
+          message: String(error)
+        })
       }
     },
     [cancelRef, setState]
-  );
+  )
 
   useEffect(() => {
-    search("");
+    search("")
     return () => {
-      cancelRef.current?.abort();
-    };
-  }, []);
+      cancelRef.current?.abort()
+    }
+  }, [])
 
   return {
     state: state,
-    search: search,
-  };
+    search: search
+  }
 }
 
-async function performSearch(searchText: string, signal: AbortSignal): Promise<SearchResult[]> {
-  const params = new URLSearchParams();
+async function performSearch(
+  searchText: string,
+  signal: AbortSignal
+): Promise<SearchResult[]> {
+  const params = new URLSearchParams()
 
-  params.append("keyword", searchText.length === 0 ? "@raycast/api" : searchText);
+  params.append(
+    "keyword",
+    searchText.length === 0 ? "@raycast/api" : searchText
+  )
 
   const response = await fetch(SEARCHURL + "?" + params.toString(), {
     method: "get",
-    signal: signal,
-  });
+    signal: signal
+  })
 
-  const start = STARTURL + "?" + "session" + "=";
+  const start = STARTURL + "?" + "session" + "="
 
   const json = (await response.json()) as
     | {
-      items: {
-        icon: { path: string };
-        title: string;
-        subtitle: string;
-        arg: string;
-        valid: number;
-        url: string;
-      }[];
-    }
-    | { code: string; message: string };
+        items: {
+          icon: { path: string }
+          title: string
+          subtitle: string
+          arg: string
+          valid: number
+          url: string
+        }[]
+      }
+    | {
+        code: string
+        message: string
+      }
 
   if (!response.ok || "message" in json) {
-    throw new Error("message" in json ? json.message : response.statusText);
+    throw new Error("message" in json ? json.message : response.statusText)
   }
 
   return json.items.map((result) => {
@@ -143,21 +169,21 @@ async function performSearch(searchText: string, signal: AbortSignal): Promise<S
       subtitle: result.subtitle,
       arg: result.arg,
       valid: result.valid,
-      url: start + result.arg,
-    };
-  });
+      url: start + result.arg
+    }
+  })
 }
 
 interface SearchState {
-  items: SearchResult[];
-  isLoading: boolean;
+  items: SearchResult[]
+  isLoading: boolean
 }
 
 interface SearchResult {
-  icon: { path: string };
-  title: string;
-  subtitle: string;
-  arg: string;
-  valid: number;
-  url: string;
+  icon: { path: string }
+  title: string
+  subtitle: string
+  arg: string
+  valid: number
+  url: string
 }
